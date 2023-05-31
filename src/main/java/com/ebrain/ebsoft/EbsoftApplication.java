faa912123384a1e2f24d282b435553989fd34080
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-06-01 00:31:58 +0900

    feat: componentScan 어노테이션 추가

--- src/main/java/com/ebrain/ebsoft/EbsoftApplication.java
+++ src/main/java/com/ebrain/ebsoft/EbsoftApplication.java
@@ -2,8 +2,10 @@ package com.ebrain.ebsoft;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.study")
 public class EbsoftApplication {
 
     public static void main(String[] args) {
