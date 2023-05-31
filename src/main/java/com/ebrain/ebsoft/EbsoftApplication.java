6350b31e542f54913813272804b0c20f07cdfb0b
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-06-01 02:17:27 +0900

    fix: ComponentScan 패키지 경로 변경

--- src/main/java/com/ebrain/ebsoft/EbsoftApplication.java
+++ src/main/java/com/ebrain/ebsoft/EbsoftApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.study")
+@ComponentScan(basePackages = {"com.study.ebsoft"})
 public class EbsoftApplication {
 
     public static void main(String[] args) {
