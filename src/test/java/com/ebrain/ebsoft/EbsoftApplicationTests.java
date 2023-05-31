e25ac80b04edb3ce01b26156744b98e3a37a5e18
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-05-31 20:25:53 +0900

    docs: 스프링 부트 초기 설정

---
+++ src/test/java/com/ebrain/ebsoft/EbsoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebrain.ebsoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbsoftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
