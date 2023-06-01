07618f684b6adf348728af2c8c7ad373560c4056
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-06-01 16:16:25 +0900

    fix: dataSourceInitializer 관련 코드 정리

--- src/main/java/com/study/ebsoft/context/DatabaseContextLoaderListener.java
+++ src/main/java/com/study/ebsoft/context/DatabaseContextLoaderListener.java
@@ -1,11 +1,9 @@
 package com.study.ebsoft.context;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.init.DataSourceInitializer;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
@@ -13,18 +11,7 @@ import javax.sql.DataSource;
 
 @Slf4j
 @Configuration
-public class CustomContextLoaderListener {
-
-    @Bean
-    public DataSource dataSource() {
-        return DataSourceBuilder
-                .create()
-                .driverClassName("com.mysql.jdbc.Driver")
-                .url("jdbc:mysql://localhost:3316/ebsoft")
-                .username("ebsoft")
-                .password("123456")
-                .build();
-    }
+public class DatabaseContextLoaderListener {
 
     @Bean
     public DataSourceInitializer dataSourceInitializer(DataSource dataSource) {
@@ -36,22 +23,6 @@ public class CustomContextLoaderListener {
         initializer.setDatabasePopulator(populator);
 
         return initializer;
-    }
-
-    @Bean
-    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
-        return new JdbcTemplate(dataSource);
-    }
-
-    @Slf4j
-    @Configuration
-    public static class ServletContextListenerConfiguration {
-
-        @Bean
-        public CustomContextLoaderListener contextLoaderListener() {
-            return new CustomContextLoaderListener();
-        }
-
     }
 
 }
