7e75d7681c8977f9e084343bb9ff453eeb66f074
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-06-03 03:05:32 +0900

    fix: Mapper 수정 및 컬럼명 중복 이슈 해결

--- src/main/java/com/study/ebsoft/mapper/BoardMapper.java
+++ src/main/java/com/study/ebsoft/mapper/BoardMapper.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Mapper
 public interface BoardMapper {
 
-    List<BoardDTO> selectBoards();
+    List<BoardDTO> selectBoardsWithFileCheck();
 
     BoardDTO selectBoard(Long boardIdx);
 
