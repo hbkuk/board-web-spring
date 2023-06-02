4911dd05a4bd00f89467b8dac88c70a126504659
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-06-02 17:44:40 +0900

    feat: Mapper.xml 과 Mapper 인터페이스 추가

--- src/main/java/com/study/ebsoft/mapper/BoardMapper.java
+++ src/main/java/com/study/ebsoft/mapper/BoardMapper.java
@@ -1,9 +1,43 @@
 package com.study.ebsoft.mapper;
 
 import com.study.ebsoft.dto.BoardDTO;
+import com.study.ebsoft.dto.CategoryDTO;
+import com.study.ebsoft.dto.CommentDTO;
+import com.study.ebsoft.dto.FileDTO;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface BoardMapper {
-    BoardDTO showTestBoard(Long boardIdx);
+
+    List<BoardDTO> selectBoards();
+
+    BoardDTO selectBoard(Long boardIdx);
+
+    BoardDTO selectBoardWithDetails(Long boardIdx);
+
+    BoardDTO selectBoardWithImages(Long boardIdx);
+
+    CommentDTO selectComment(Long commentIdx);
+
+    List<CategoryDTO> selectAllCategory();
+
+    List<Long> selectFileIndexes(Long boardIdx);
+
+    String selectSavedFileName(Long fileIdx);
+
+    void insertBoard(BoardDTO board);
+
+    void insertFile(FileDTO file);
+
+    void insertComment(CommentDTO commentDTO);
+
+    int updateBoard(BoardDTO boardDTO);
+
+    int increaseHit(Long boardIdx);
+
+    int deleteFile(Long fileIdx);
+
+    int deleteComment(Long commentIdx);
 }
