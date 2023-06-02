263c68615439b3af0d36ae7f980811897f0b6f46
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-06-02 22:59:11 +0900

    docs: DB. 테스트 insert 수정

--- src/main/resources/initdb.sql
+++ src/main/resources/initdb.sql
@@ -75,35 +75,31 @@ INSERT INTO tb_category (category_code, category) VALUES
 
 
 INSERT INTO tb_board (category_idx, title, writer, content, password, hit, regdate, moddate) VALUES
-                                                                                                 (1, 'Title 1', '테스터1', 'Content 1', 'password1!', 0, '2023-05-18 10:00:00', null),
-                                                                                                 (2, 'Title 2', '테스터2', 'Content 2', 'password2!', 5, '2023-05-18 11:00:00', null),
-                                                                                                 (3, 'Title 3', '테스터3', 'Content 3', 'password3!', 8, '2023-05-18 12:00:00', null),
-                                                                                                 (4, 'Title 4', '테스터4', 'Content 4', 'password4!', 3, '2023-05-18 13:00:00', null),
-                                                                                                 (5, 'Title 5', '테스터5', 'Content 5', 'password5!', 15, '2023-05-18 14:00:00', null),
-                                                                                                 (6, 'Title 6', '테스터6', 'Content 6', 'password6!', 12, '2023-05-18 15:00:00', null),
-                                                                                                 (7, 'Title 7', '테스터7', 'Content 7', 'password7!', 7, '2023-05-18 16:00:00', null),
-                                                                                                 (8, 'Title 8', '테스터8', 'Content 8', 'password8!', 20, '2023-05-18 17:00:00', null),
-                                                                                                 (9, 'Title 9', '테스터9', 'Content 9', 'password9!', 2, '2023-05-18 18:00:00', null),
-                                                                                                 (10, 'Title 10', '테스터10', 'Content 10', 'password10!', 9, '2023-05-18 19:00:00', null);
+                                                                                                 (1, 'Title 1', '테1', 'Content 1', 'password1!', 0, '2023-05-18 10:00:00', null),
+                                                                                                 (2, 'Title 2', '테2', 'Content 2', 'password2!', 5, '2023-05-18 11:00:00', null),
+                                                                                                 (3, 'Title 3', '테3', 'Content 3', 'password3!', 8, '2023-05-18 12:00:00', null),
+                                                                                                 (4, 'Title 4', '테4', 'Content 4', 'password4!', 3, '2023-05-18 13:00:00', null),
+                                                                                                 (5, 'Title 5', '테5', 'Content 5', 'password5!', 15, '2023-05-18 14:00:00', null),
+                                                                                                 (6, 'Title 6', '테6', 'Content 6', 'password6!', 12, '2023-05-18 15:00:00', null),
+                                                                                                 (7, 'Title 7', '테7', 'Content 7', 'password7!', 7, '2023-05-18 16:00:00', null),
+                                                                                                 (8, 'Title 8', '테8', 'Content 8', 'password8!', 20, '2023-05-18 17:00:00', null),
+                                                                                                 (9, 'Title 9', '테9', 'Content 9', 'password9!', 2, '2023-05-18 18:00:00', null),
+                                                                                                 (10, 'Title 10', '테10', 'Content 10', 'password10!', 9, '2023-05-18 19:00:00', null);
 
 
 INSERT INTO tb_comment (writer, password, content, regdate, board_idx) VALUES
-                                                                           ('사용자1', 'commentpass1!', 'Comment 1', '2023-05-18 10:30:00', 1),
-                                                                           ('사용자1', 'commentpass1!', 'Comment 1', '2023-05-18 10:30:00', 1),
-                                                                           ('사용자1', 'commentpass1!', 'Comment 1', '2023-05-18 10:30:00', 1),
-                                                                           ('사용자2', 'commentpass2!', 'Comment 2', '2023-05-18 11:30:00', 2),
-                                                                           ('사용자3', 'commentpass3!', 'Comment 3', '2023-05-18 12:30:00', 3),
-                                                                           ('사용자4', 'commentpass4!', 'Comment 4', '2023-05-18 13:30:00', 4),
-                                                                           ('사용자5', 'commentpass5!', 'Comment 5', '2023-05-18 14:30:00', 5),
-                                                                           ('사용자6', 'commentpass6!', 'Comment 6', '2023-05-18 15:30:00', 6),
-                                                                           ('사용자7', 'commentpass7!', 'Comment 7', '2023-05-18 16:30:00', 7),
-                                                                           ('사용자8', 'commentpass8!', 'Comment 8', '2023-05-18 17:30:00', 8),
-                                                                           ('사용자9', 'commentpass9!', 'Comment 9', '2023-05-18 18:30:00', 9),
-                                                                           ('사용자10', 'commentpass10!', 'Comment 10', '2023-05-18 19:30:00', 10);
+                                                                           ('테1', 'commentpass1!', 'Comment 1', '2023-05-18 10:30:00', 1),
+                                                                           ('테2', 'commentpass2!', 'Comment 2', '2023-05-18 11:30:00', 2),
+                                                                           ('테3', 'commentpass3!', 'Comment 3', '2023-05-18 12:30:00', 3),
+                                                                           ('테4', 'commentpass4!', 'Comment 4', '2023-05-18 13:30:00', 4),
+                                                                           ('테5', 'commentpass5!', 'Comment 5', '2023-05-18 14:30:00', 5),
+                                                                           ('테6', 'commentpass6!', 'Comment 6', '2023-05-18 15:30:00', 6),
+                                                                           ('테7', 'commentpass7!', 'Comment 7', '2023-05-18 16:30:00', 7),
+                                                                           ('테8', 'commentpass8!', 'Comment 8', '2023-05-18 17:30:00', 8),
+                                                                           ('테9', 'commentpass9!', 'Comment 9', '2023-05-18 18:30:00', 9),
+                                                                           ('테10', 'commentpass10!', 'Comment 10', '2023-05-18 19:30:00', 10);
 
 INSERT INTO tb_file (saved_name, original_name, size, board_idx) VALUES
-                                                                     ('file1.png', 'Image 1.png', 1024, 1),
-                                                                     ('file1.png', 'Image 1.png', 1024, 1),
                                                                      ('file1.png', 'Image 1.png', 1024, 1),
                                                                      ('file2.png', 'Image 2.png', 2048, 2),
                                                                      ('file3.png', 'Image 3.png', 3072, 3),
