d268e3f1e6f7d7e89d50b7bdfd8ab72138c83c1e
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-06-02 17:43:30 +0900

    fix: 모든 DTO 기본 생성자 및 빌더패턴 추가

--- src/main/java/com/study/ebsoft/dto/CommentDTO.java
+++ src/main/java/com/study/ebsoft/dto/CommentDTO.java
@@ -1,5 +1,6 @@
 package com.study.ebsoft.dto;
 
+import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,4 +16,16 @@ public class CommentDTO {
     private LocalDateTime regDate;
     private long boardIdx;
 
+    public CommentDTO() {
+    }
+
+    @Builder
+    public CommentDTO(Long commentIdx, String writer, String password, String content, LocalDateTime regDate, long boardIdx) {
+        this.commentIdx = commentIdx;
+        this.writer = writer;
+        this.password = password;
+        this.content = content;
+        this.regDate = regDate;
+        this.boardIdx = boardIdx;
+    }
 }
--- src/main/java/com/study/ebsoft/dto/FileDTO.java
+++ src/main/java/com/study/ebsoft/dto/FileDTO.java
@@ -1,8 +1,8 @@
 package com.study.ebsoft.dto;
 
+import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 @Data
 public class FileDTO {
@@ -10,5 +10,17 @@ public class FileDTO {
     private String savedFileName;
     private String originalFileName;
     private int fileSize;
-    private long BoardIdx;
+    private long boardIdx;
+
+    public FileDTO() {
+    }
+
+    @Builder
+    public FileDTO(long fileIdx, String savedFileName, String originalFileName, int fileSize, long boardIdx) {
+        this.fileIdx = fileIdx;
+        this.savedFileName = savedFileName;
+        this.originalFileName = originalFileName;
+        this.fileSize = fileSize;
+        this.boardIdx = boardIdx;
+    }
 }
