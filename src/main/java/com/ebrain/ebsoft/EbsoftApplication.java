d4c09af6586d2f8e59675c9959fd144d00bae60b
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-06-02 10:36:31 +0900

    feat: MapperScan 어노테이션 추가

--- src/main/java/com/ebrain/ebsoft/EbsoftApplication.java
+++ src/main/java/com/ebrain/ebsoft/EbsoftApplication.java
@@ -1,11 +1,13 @@
 package com.ebrain.ebsoft;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.study.ebsoft"})
+@MapperScan("com.study.ebsoft.mapper")
 public class EbsoftApplication {
 
     public static void main(String[] args) {
