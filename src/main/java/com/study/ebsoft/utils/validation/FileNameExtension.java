2b0f0d108c6eec9fec13d81e84e97cc5384e154e
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-06-06 00:52:36 +0900

    feat: File 확장자 관련 Enum 추가

---
+++ src/main/java/com/study/ebsoft/utils/validation/FileNameExtension.java
@@ -0,0 +1,14 @@
+package com.study.ebsoft.utils.validation;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public enum FileNameExtension {
+    PNG, JPEG, BMP, GIF, JPG, DOCS;
+
+    public static boolean contains(String extension) {
+        List<String> extensions = Arrays.asList(values()).stream().map(Enum::name).collect(Collectors.toList());
+        return extensions.contains(extension);
+    }
+}
