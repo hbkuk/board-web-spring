ac2e707de60ae0ccca446ad1cef083aee7fdeaa5
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-06-08 03:05:28 +0900

    feat: 패스워드가 다름을 의미하는 커스텀 Exception 예외 클래스 추가

---
+++ src/main/java/com/study/ebsoft/exception/InvalidPasswordException.java
@@ -0,0 +1,8 @@
+package com.study.ebsoft.exception;
+
+public class InvalidPasswordException extends RuntimeException {
+
+    public InvalidPasswordException(String message) {
+        super(message);
+    }
+}
