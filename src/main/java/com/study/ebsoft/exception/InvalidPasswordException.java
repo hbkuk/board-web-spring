c57e358cfcd9515a72c1f7956aa977a7331c18e1
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-06-08 11:57:43 +0900

    feat:  잘못된 비밀번호가 전달되었을 때 던져지는 Unchecked Exception 클래스 추가

--- src/main/java/com/study/ebsoft/exception/InvalidPasswordException.java
+++ src/main/java/com/study/ebsoft/exception/InvalidPasswordException.java
@@ -1,5 +1,8 @@
 package com.study.ebsoft.exception;
 
+/**
+ * 잘못된 비밀번호가 전달되었을 때 던져지는 Unchecked Exception 입니다.
+ */
 public class InvalidPasswordException extends RuntimeException {
 
     public InvalidPasswordException(String message) {
