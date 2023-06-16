702e9caa62b6f17d2942c5cf3b05232b78139736
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-06-17 01:10:47 +0900

    feat: 유효성 검증시 던져지는 예외를 핸들링하는 메서드 추가

--- src/main/java/com/study/ebsoft/handler/GlobalExceptionHandler.java
+++ src/main/java/com/study/ebsoft/handler/GlobalExceptionHandler.java
@@ -1,20 +1,67 @@
 package com.study.ebsoft.handler;
 
+import com.study.ebsoft.exception.ErrorCode;
+import com.study.ebsoft.exception.ErrorResponse;
 import com.study.ebsoft.exception.SearchConditionException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.UnsupportedEncodingException;
+import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 @Slf4j
 @ControllerAdvice
+@RestController
 public class GlobalExceptionHandler {
+
+    /**
+     * MethodArgumentNotValidException을 처리하는 예외 핸들러입니다.
+     * 메서드 인자의 유효성 검증 오류를 처리합니다.
+     *
+     * @param e MethodArgumentNotValidException 인스턴스
+     * @return ErrorResponse와 HttpStatus를 포함하는 ResponseEntity
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
+        ErrorResponse errorResponse = new ErrorResponse(ErrorCode.INVALID_BOARD_DATA);
+
+        List<String> fieldErrors = e.getAllErrors().stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+        errorResponse.setDetail(String.join(", ", fieldErrors));
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    /**
+     * BindException을 처리하는 예외 핸들러입니다.
+     * 바인딩 오류를 처리합니다.
+     *
+     * @param e BindException 인스턴스
+     * @return ErrorResponse와 HttpStatus를 포함하는 ResponseEntity
+     */
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity<ErrorResponse> handleBindException(BindException e) {
+        ErrorResponse errorResponse = new ErrorResponse(ErrorCode.INVALID_SEARCH_CONDITION);
+
+        List<String> fieldErrors = e.getAllErrors().stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+        errorResponse.setDetail(String.join(", ", fieldErrors));
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
 
     /**
      * 쿼리 스트링에 포함된 검색 조건이 유효하지 않은 경우 해당 예외 처리를 담당합니다.
@@ -49,19 +96,6 @@ public class GlobalExceptionHandler {
         log.error(e.getMessage());
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body("해당 글을 찾을 수 없습니다."); // Status Code 404
-    }
-
-    /**
-     * 최상위 예외 처리
-     *
-     * @param e 발생한 Throwable 예외 객체
-     * @return RuntimeException 발생을 위한 예외 처리
-     */
-    @ExceptionHandler(Throwable.class)
-    public ModelAndView handleThrowable(Throwable e) {
-        log.error(e.getMessage());
-
-        throw new RuntimeException("Internal Server Error");
     }
 }
 
